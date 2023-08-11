d1c0a1efea1809cce12bc3373260cb995d0d1d0b
phunglam98 <duclam9812@gmail.com>
2023-08-11 15:37:51 +0700

    tao file index

--- quanlisanpham/js/view/main.js
+++ quanlisanpham/js/view/main.js
@@ -1,24 +1,24 @@
 let store = new Store
-function main() {
-    let product = new Product(1,'bánh', 3 , 1000)
-    store.add(product);
-    let productInStore = store.findAll()
-    for (let i = 0; i < productInStore.length; i++) {
-        console.log(productInStore[i]);
-
-    }
-
-}
-main()
+// function main() {
+//     let product = new Product(1,'bánh', 3 , 1000)
+//     store.add(product);
+//     let productInStore = store.findAll()
+//     for (let i = 0; i < productInStore.length; i++) {
+//         console.log(productInStore[i]);
+//
+//     }
+//
+// }
+// main()
 let productInStore = store.findAll()
 function showAll() {
     let str = ``;
     for (let i = 0; i < productInStore.length; i++) {
         str += `<tr>
-            <td>${productInStore[i]._id}</td>
-            <td>${productInStore[i]._name}</td>
-            <td>${productInStore[i]._quality}</td>
-            <td>${productInStore[i]._price}</td>
+            <td>${productInStore[i].id}</td>
+            <td>${productInStore[i].name}</td>
+            <td>${productInStore[i].quality}</td>
+            <td>${productInStore[i].price}</td>
             <td><button>edit</button></td>
             <td><button onclick = "remove(${i})">remove</button></td>
         </tr>`
@@ -29,7 +29,7 @@ function showAll() {
 }
 showAll()
 
-function showFormAdd() {
+function add() {
     let id = document.getElementById('id').value;
     let name = document.getElementById('name').value;
     let quality = document.getElementById('quality');
@@ -37,12 +37,44 @@ function showFormAdd() {
     let newProduct = new Product(id, name, quality, price);
     store.add(newProduct);
     console.log(store);
-    alert("Add Success");
+    // alert("Add Success");
     showAll();
     document.getElementById('form-add').innerHTML = '';
 }
+function showFormAdd() {
+
+    document.getElementById('form-add').innerHTML =
+        `
+            <center>
+        <h1>Add Product</h1>
+        <table>
+            <tr>
+                <td>Id</td>
+                <td><input type="number" id="id"></td>
+            </tr>
+            <tr>
+                <td>Name</td>
+                <td><input type="text" id="name"></td>
+            </tr>
+            <tr>
+                <td>Quality</td>
+                <td><input type="number" id="quality"></td>
+            </tr>
+            <tr>
+                <td>Price</td>
+                <td><input type="number" id="price"></td>
+            </tr>
+            <tr>
+                <th colspan="2">
+                    <button onclick="add()">Add</button>
+                </th>
+            </tr>
+        </table>
+    </center>
+    `
+}
 function remove(index) {
-    let isConfirm = confirm('Bạn có muốn xóa không ?')
+    let isConfirm = confirm('xóa')
     if (isConfirm) {
         store.remove(index)
         showAll();
